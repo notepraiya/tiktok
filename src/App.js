da9efa7d303d49f1eae16545379f9fee8bdb419e
notepraiya <notepraiya@gmail.com>
2020-12-13 20:06:23 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import iconFont from 'react-native-vector-icons/Fonts/FontAwesome.ttf';
+
+import AntDesign from 'react-native-vector-icons/dist/AntDesign';
+import antFont from 'react-native-vector-icons/Fonts/AntDesign.ttf';
+
+const iconFontStyles = `
+@font-face {
+  src: url(${iconFont});
+  font-family: FontAwesome;
+}
+@font-face {
+  src: url(${antFont});
+  font-family: AntDesign;
+}
+`;
+const style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet) {
+  style.styleSheet.cssText = iconFontStyles;
+} else {
+  style.appendChild(document.createTextNode(iconFontStyles));
+}
+document.head.appendChild(style);
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+};
+
+export default App;
